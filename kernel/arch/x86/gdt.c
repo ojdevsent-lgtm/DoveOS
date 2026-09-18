cf6a02f045330bd86ea5d4199f08875213b08a7f
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 19:17:32 +0100

    feat: implement x86 GDT

---
+++ kernel/arch/x86/gdt.c
@@ -0,0 +1,43 @@
+#include "../../include/types.h"
+#include "gdt.h"
+
+struct gdt_entry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t base_middle;
+    uint8_t access;
+    uint8_t granularity;
+    uint8_t base_high;
+} __attribute__((packed));
+
+struct gdt_ptr {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
+
+static struct gdt_entry gdt[3];
+static struct gdt_ptr gp;
+
+extern void gdt_flush(uint32_t address);
+
+static void gdt_set_gate(uint32_t index, uint32_t base, uint32_t limit,
+                         uint8_t access, uint8_t granularity) {
+    gdt[index].base_low = (uint16_t)(base & 0xFFFF);
+    gdt[index].base_middle = (uint8_t)((base >> 16) & 0xFF);
+    gdt[index].base_high = (uint8_t)((base >> 24) & 0xFF);
+    gdt[index].limit_low = (uint16_t)(limit & 0xFFFF);
+    gdt[index].granularity = (uint8_t)((limit >> 16) & 0x0F);
+    gdt[index].granularity |= granularity & 0xF0;
+    gdt[index].access = access;
+}
+
+void gdt_init(void) {
+    gp.limit = (uint16_t)(sizeof(gdt) - 1);
+    gp.base = (uint32_t)&gdt;
+
+    gdt_set_gate(0, 0, 0, 0, 0);
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+
+    gdt_flush((uint32_t)&gp);
+}
