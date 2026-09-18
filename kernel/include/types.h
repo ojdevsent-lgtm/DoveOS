dca1e8302d725baae0b3cad7d86a16fe9e8f908f
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-18 19:17:02 +0100

    fix: add freestanding kernel integer types

---
+++ kernel/include/types.h
@@ -0,0 +1,11 @@
+#ifndef DOVE_TYPES_H
+#define DOVE_TYPES_H
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+
+typedef unsigned int size_t;
+
+#endif
